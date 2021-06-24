1d575c100ef60223b4e4249796381e236d2eaa57
tunnavacher <37768508+tunnavacher@users.noreply.github.com>
2021-06-24 16:36:36 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,9 @@
+# Lambda function_name
+resource "aws_lambda_function" "test_lambda" {
+  function_name = var.function_name
+  description   = "My awesome lambda function"
+  handler       = var.lambda_handler
+  runtime       = var.runtime
+  role = var.lambda_execution_role
+  s3_bucket = var.s3_bucket
+  s3_key = var.s3_key
