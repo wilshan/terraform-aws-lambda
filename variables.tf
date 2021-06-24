736e5f02bf708694d88b1fb3cb8a78a61c6bbd3b
tunnavacher <37768508+tunnavacher@users.noreply.github.com>
2021-06-24 16:31:40 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,34 @@
+Mylambdatest
+variable "function_name" {
+  type = string
+  default = ""
+}
+index.lambda_handler
+variable "lambda_handler" {
+  type = string
+  default = ""
+}
+"python3.8"
+variable "runtime" {
+  type = string
+  default = ""
+}
+
+variable "lambda_execution_role" {
+  type = string
+  default = ""
+}
+
+s3_bucket
+
+variable "s3_bucket" {
+  type = string
+  default = ""
+}
+
+"lambdacode.zip"
+
+variable "s3_key" {
+  type = string
+  default = ""
+}
