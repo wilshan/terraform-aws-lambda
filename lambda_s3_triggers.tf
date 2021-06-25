f2c963e21a65b0b1118b173a6f1069422aaaf476
tunnavacher <37768508+tunnavacher@users.noreply.github.com>
2021-06-25 15:32:11 +0530

    Update lambda_s3_triggers.tf

--- lambda_s3_triggers.tf
+++ lambda_s3_triggers.tf
@@ -1,15 +1,3 @@
-#resource "aws_s3_bucket_notification" "lambda-trigger" {
-  #  bucket = var.event_bucket_name
-
-   # lambda_function {
-     #   lambda_function_arn = var.aws_lambda_function
-     #   events              = [var.event_type]
-      #  filter_prefix       = var.prefix
-    #   filter_suffix       = var.suffix
-   # }
-#}
-
-
 # Adding S3 bucket as trigger to my lambda and giving the permissions
 ##################
 resource "aws_s3_bucket_notification" "aws-lambda-trigger" {
@@ -23,9 +11,9 @@ filter_suffix       = "file-extension"
 }
 
 resource "aws_lambda_permission" "test" {
-statement_id  = "AllowS3Invoke"
-action        = "lambda:InvokeFunction"
+statement_id  = var.statement_id
+action        = var.action
 function_name = "${aws_lambda_function.test_lambda.function_name}"
-principal = "s3.amazonaws.com"
-source_arn = "arn:aws:s3:::landlingbucketlakeformation"
+principal = var.principle
+source_arn = var.source_arn
 }
