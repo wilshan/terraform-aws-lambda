cb0006c4ac72fe9ca90c33b97422b11162c0a4fa
tunnavacher <37768508+tunnavacher@users.noreply.github.com>
2021-06-25 15:47:03 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -6,10 +6,40 @@ output "lambda_function_role" {
   value = aws_lambda_function.test_lambda.role
 }
 
+output "lambda_function_arn" {
+  value = aws_lambda_function.test_lambda.arn
+}
+
+output "lambda_function_runtime" {
+  value = aws_lambda_function.test_lambda.runtime
+}
+
 output "lambda_function_layers" {
   value = aws_lambda_function.test_lambda.layers
 }
 
-output "lambda_function_arn" {
-  value = aws_lambda_function.test_lambda.arn
+output "lambda_function_timeout" {
+  value = aws_lambda_function.test_lambda.timeout
 }
+
+output "lambda_function_version" {
+  value = aws_lambda_function.test_lambda.version
+}
+
+output "lambda_function_action" {
+  value = aws_lambda_function.test_lambda.action
+}
+
+output "lambda_function_principal" {
+  value = aws_lambda_function.test_lambda.principal
+}
+ 
+output "lambda_function_statement_id" {
+  value = aws_lambda_function.test_lambda.statement_id
+}
+
+output "lambda_function_source_arn" {
+  value = aws_lambda_function.test_lambda.statement_source_arn
+}
+
+
