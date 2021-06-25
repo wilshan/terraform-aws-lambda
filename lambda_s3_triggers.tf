5b91e95394f2d2c25dae83a302659a7739422ffe
tunnavacher <37768508+tunnavacher@users.noreply.github.com>
2021-06-25 15:24:26 +0530

    Update lambda_s3_triggers.tf

--- lambda_s3_triggers.tf
+++ lambda_s3_triggers.tf
@@ -21,3 +21,11 @@ filter_prefix       = "file-prefix"
 filter_suffix       = "file-extension"
 }
 }
+
+resource "aws_lambda_permission" "test" {
+statement_id  = "AllowS3Invoke"
+action        = "lambda:InvokeFunction"
+function_name = "${aws_lambda_function.test_lambda.function_name}"
+principal = "s3.amazonaws.com"
+source_arn = "arn:aws:s3:::landlingbucketlakeformation"
+}
