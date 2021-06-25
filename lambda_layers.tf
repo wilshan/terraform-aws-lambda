0ee5ea9d169e49512148ae02846888f5205c755f
tunnavacher <37768508+tunnavacher@users.noreply.github.com>
2021-06-25 12:11:22 +0530

    Update lambda_layers.tf

--- lambda_layers.tf
+++ lambda_layers.tf
@@ -1,6 +1,8 @@
 resource "aws_lambda_layer_version" "lambda_layer" {
-  filename   = var.lambda_layer_file
+  #filename   = var.lambda_layer_file
   layer_name = var.lambda_layer_name
+  s3_bucket = var.s3_bucket
+ s3_key = var.s3_key
 
   compatible_runtimes = [var.layer_runtime]
 }
