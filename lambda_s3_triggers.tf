06ddf9aca8e9bccd584997f18c7fab5e7caf901b
tunnavacher <37768508+tunnavacher@users.noreply.github.com>
2021-06-25 16:13:31 +0530

    Update lambda_s3_triggers.tf

--- lambda_s3_triggers.tf
+++ lambda_s3_triggers.tf
@@ -3,17 +3,17 @@
 resource "aws_s3_bucket_notification" "aws-lambda-trigger" {
 bucket = var.event_bucket_name
 lambda_function {
-lambda_function_arn = "${aws_lambda_function.test_lambda.arn}"
-events              = ["s3:ObjectCreated:*"]
-filter_prefix       = "file-prefix"
-filter_suffix       = "file-extension"
+lambda_function_arn = aws_lambda_function.test_lambda.arn
+events              = var.event_type
+filter_prefix       = var.prefix
+filter_suffix       = var.suffix
 }
 }
 
 resource "aws_lambda_permission" "test" {
 statement_id  = var.statement_id
 action        = var.action
-function_name = "${aws_lambda_function.test_lambda.function_name}"
+function_name = aws_lambda_function.test_lambda.function_name
 principal = var.principle
 source_arn = var.source_arn
 }
