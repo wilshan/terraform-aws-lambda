0d0bec6c32b3a479715272e5329f2d2e2f32a92b
tunnavacher <37768508+tunnavacher@users.noreply.github.com>
2021-06-25 15:02:30 +0530

    Update lambda_s3_triggers.tf

--- lambda_s3_triggers.tf
+++ lambda_s3_triggers.tf
@@ -4,7 +4,7 @@ resource "aws_s3_bucket_notification" "lambda-trigger" {
     lambda_function {
         lambda_function_arn = "${var.aws_lambda_function}"
         events              = [var.event_type]
-        filter_prefix       = var.prefix
-       filter_suffix       = var.suffix
+      #  filter_prefix       = var.prefix
+    #   filter_suffix       = var.suffix
     }
 }
