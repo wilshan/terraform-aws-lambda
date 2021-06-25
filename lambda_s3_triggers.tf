c8e654c362f632d45191243e5e9c1d2148ca2ed1
tunnavacher <37768508+tunnavacher@users.noreply.github.com>
2021-06-25 15:12:25 +0530

    Update lambda_s3_triggers.tf

--- lambda_s3_triggers.tf
+++ lambda_s3_triggers.tf
@@ -1,10 +1,23 @@
-resource "aws_s3_bucket_notification" "lambda-trigger" {
-    bucket = var.event_bucket_name
+#resource "aws_s3_bucket_notification" "lambda-trigger" {
+  #  bucket = var.event_bucket_name
 
-    lambda_function {
-        lambda_function_arn = var.aws_lambda_function
-        events              = [var.event_type]
+   # lambda_function {
+     #   lambda_function_arn = var.aws_lambda_function
+     #   events              = [var.event_type]
       #  filter_prefix       = var.prefix
     #   filter_suffix       = var.suffix
-    }
+   # }
+#}
+
+
+# Adding S3 bucket as trigger to my lambda and giving the permissions
+##################
+resource "aws_s3_bucket_notification" "aws-lambda-trigger" {
+bucket = var.event_bucket_name
+lambda_function {
+lambda_function_arn = "${aws_lambda_function.test_lambda.arn}"
+events              = ["s3:ObjectCreated:*"]
+filter_prefix       = "file-prefix"
+filter_suffix       = "file-extension"
+}
 }
