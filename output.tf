ddb45d143f94028a4aae879a4ddfe0a722186f48
tunnavacher <37768508+tunnavacher@users.noreply.github.com>
2021-06-25 13:00:44 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -5,3 +5,7 @@ output "lambda_function_name" {
 output "lambda_function_role" {
   value = aws_lambda_function.test_lambda.role
 }
+
+output "lambda_function_layers" {
+  value = aws_lambda_function.test_lambda.layers
+}
