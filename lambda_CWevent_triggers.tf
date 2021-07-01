a94dba2394ac236ed4edc0d157b24d9f25019c96
tunnavacher <37768508+tunnavacher@users.noreply.github.com>
2021-07-01 14:05:23 +0530

    Update lambda_CWevent_triggers.tf

--- lambda_CWevent_triggers.tf
+++ lambda_CWevent_triggers.tf
@@ -1,21 +1,21 @@
 # Adding Cloudwatch events as trigger to my lambda and giving the permissions
 ##################
 resource "aws_cloudwatch_event_rule" "lambda_trigger_every_five_minutes" {
-    name = "lambda_trigger_every-five-minutes"
+    name = var.cwrulename
     description = "Fires every five minutes"
-    schedule_expression = "rate(5 minutes)"
+    schedule_expression = var.Schedule
 }
 
 resource "aws_cloudwatch_event_target" "lambda_trigger_every_five_minutes" {
     rule = "${aws_cloudwatch_event_rule.lambda_trigger_every_five_minutes.name}"
-    target_id = "test_lambda"
+    target_id = var.target_id
     arn = aws_lambda_function.test_lambda.arn
 }
 
 resource "aws_lambda_permission" "allow_cloudwatch_to_call_test_lambda" {
-    statement_id = "AllowExecutionFromCloudWatch"
-    action = "lambda:InvokeFunction"
+    statement_id = var.statement_id2
+    action = var.action2
     function_name = aws_lambda_function.test_lambda.function_name
-    principal = "events.amazonaws.com"
+    principal = var.principle2
     source_arn = "${aws_cloudwatch_event_rule.lambda_trigger_every_five_minutes.arn}"
 }
