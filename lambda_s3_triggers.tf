5575d57f7a364b1237d23b7f6ac716dc8830b865
wilshan <74950445+wilshan@users.noreply.github.com>
2021-07-02 15:30:24 +0530

    Update lambda_s3_triggers.tf

--- lambda_s3_triggers.tf
+++ lambda_s3_triggers.tf
@@ -1,19 +1,19 @@
 # Adding S3 bucket as trigger to my lambda and giving the permissions
 ##################
-resource "aws_s3_bucket_notification" "aws-lambda-trigger" {
-bucket = var.event_bucket_name
-lambda_function {
-lambda_function_arn = aws_lambda_function.test_lambda.arn
-events              = [var.event_type]
-filter_prefix       = var.prefix
-filter_suffix       = var.suffix
+#resource "aws_s3_bucket_notification" "aws-lambda-trigger" {
+##bucket = var.event_bucket_name
+#lambda_function {
+#lambda_function_arn = aws_lambda_function.test_lambda.arn
+#events              = [var.event_type]
+#filter_prefix       = var.prefix
+#filter_suffix       = var.suffix
 }
 }
 
-#resource "aws_lambda_permission" "test" {
-#statement_id  = var.statement_id1
-#action        = var.action1
-#function_name = aws_lambda_function.test_lambda.function_name
-#principal = var.principle1
-#source_arn = var.source_arn
-#}
+resource "aws_lambda_permission" "test" {
+statement_id  = var.statement_id1
+action        = var.action1
+function_name = aws_lambda_function.test_lambda.function_name
+principal = var.principle1
+source_arn = var.source_arn
+}
